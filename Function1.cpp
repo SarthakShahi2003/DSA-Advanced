f0349b62190e92506c14b08f58cafe56be62402f
Sarthak Shahi <168637240+SarthakShahi2003@users.noreply.github.com>
2025-07-09 21:41:47 +0530

    Add files via upload

---
+++ Function1.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+
+int maxnum(int a,int b,int c){
+    int ans= a+b+c;
+    return ans;
+    }
+
+int main(){
+    int sum=printsum(4,4,15);
+    cout<<"Sum is: "<<sum<<endl;
+    
+ return 0;
+ 
+}
