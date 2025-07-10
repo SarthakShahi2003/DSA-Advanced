19cc35a3b25cf35b15c53f09706cf42efbae44c7
Sarthak Shahi <168637240+SarthakShahi2003@users.noreply.github.com>
2025-07-10 16:56:04 +0530

    10th july

---
+++ Array.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+int number[15];
+
+cout<<"value at 16 index " << number[16] << endl;
+
+int second[3]={5 ,7, 11};
+
+cout<<"value at 2 index : " << second[2] << endl;
+
+cout<<endl<<"Everything is fine"<<endl;
+
+return 0;
+}
