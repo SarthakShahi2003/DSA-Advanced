76585bfd6dfd119abf2894827c424a42168f7b48
Sarthak Shahi <168637240+SarthakShahi2003@users.noreply.github.com>
2025-07-13 20:56:53 +0530

    13th July

---
+++ SumOfTwoGivenNumbers.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+int main (){
+    int x;
+    int y;
+     cout<<"enter the numbers : ";
+    cin>>x;
+    cin>>y;
+    int z=x+y;
+    cout<<" the sum of the numbers is : ";
+    cout<<z;
+}
+
+
+    
+
+
